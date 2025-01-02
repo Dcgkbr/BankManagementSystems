5fca6f3e9ef9f1e20f48f1d8022cdb4b64a3706b
Dcgkbr <azomingo47@gmail.com>
2025-01-02 13:33:36 +0300

    Initialize project and create main structure

---
+++ bank.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class BankAccount {
+public:
+    std::string name;
+    std::string accountNumber;
+    double balance;
+};
+
+class BankManagementSystem {
+private:
+    std::vector<BankAccount> accounts;
+
+public:
+    // Methods will be implemented in later commits
+};
+
+int main() {
+    // Basic structure for the main function
+    return 0;
+}
